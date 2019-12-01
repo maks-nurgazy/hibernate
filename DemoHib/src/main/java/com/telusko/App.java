542794becdd99d9cd79bee421733d8bac6a4e643
maks-nurgazy <maksatbek.bolushov@gmail.com>
2019-12-01 16:32:45 +0600

    Without deprecated hibernate

--- DemoHib/src/main/java/com/telusko/App.java
+++ DemoHib/src/main/java/com/telusko/App.java
@@ -4,20 +4,25 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 
 public class App 
 {
     public static void main( String[] args )
     {
         Alien alien = new Alien();
-        alien.setAid(101);
-        alien.setAname("maksatbek");
-        alien.setColor("Black");
+        alien.setAid(102);
+        alien.setAname("Beknur");
+        alien.setColor("White");
 
         Configuration con = new Configuration();
         con.configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class);
 
-        SessionFactory sf =con.buildSessionFactory();
+        ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+
+        SessionFactory sf =con.buildSessionFactory(reg);
         Session session = sf.openSession();
 
         Transaction tx = session.beginTransaction();
